6b815b0adc2fa244639a26ea44cb696dd9ee5fe8
0xA54 <141027176+0xA54@users.noreply.github.com>
2025-10-02 14:08:23 +1000

    starter code

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,14 @@
 use wex_robot::prelude::*;
 
 #[embassy_executor::main]
-async fn main(_spawner: embassy_executor::Spawner) {
+async fn main(spawner: embassy_executor::Spawner) {
     let mut robot = Robot::init(
-        _spawner,
+        spawner,
         "MyRobot1",
         Address::random([0xff, 0x8f, 0x1a, 0x05, 0xe4, 0xff]),
     )
     .await;
 
-    let mut state = Level::Low;
-
-    loop {
-        state = match state {
-            Level::Low => Level::High,
-            Level::High => Level::Low,
-        };
-        robot.set_led_5(state);
-
-        match COMMAND_SIGNAL.wait().await {
-            b'w' => robot.set_speeds(50, 50),
-            b'a' => robot.set_speeds(0, 25),
-            b's' => robot.set_speeds(-50, -50),
-            b'd' => robot.set_speeds(25, 0),
-            b'x' => robot.set_speeds(0, 0),
-            b'b' => robot.halt_motors(),
-            KILL => robot.halt_motors(),
-            _ => (),
-        }
-    }
+    // ------------------- START MODIFYING HERE -----------------------
+    // ----------------------------------------------------------------
 }
